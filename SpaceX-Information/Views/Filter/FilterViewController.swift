783da40f12c94bcac22cfa01ef1425aa416bf315
Owen Harper <owenharper98@gmail.com>
2021-11-16 21:35:08 +0000

    add initial view hierarchy

---
+++ SpaceX-Information/Views/Filter/FilterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FilterViewController.swift
+//  SpaceX-Information
+//
+//  Created by Owen Harper on 16/11/2021.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		decorateInterface()
+    }
+	
+	private func decorateInterface() {
+		title = "Filter"
+	}
+}
--- SpaceX-Information/Views/Information/InformationViewController.swift
+++ SpaceX-Information/Views/Information/InformationViewController.swift
@@ -9,9 +9,50 @@ import UIKit
 
 class InformationViewController: UIViewController {
 
+	private var navigator: InformationNavigator?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+		decorateInterface()
+		
+		guard let navigationController = navigationController else { return }
+		
+		navigator = .init(navigationController: navigationController)
     }
+	
+	// MARK: Set Up -
+	
+	private func decorateInterface() {
+		title = "SpaceX"
+		navigationController?.navigationBar.prefersLargeTitles = true
+		
+		let filterButton = UIBarButtonItem(image: .filterIcon, style: .plain, target: self, action: #selector(presentFilterView))
+		navigationItem.rightBarButtonItem = filterButton
+	}
+	
+	// MARK: Actions -
+	
+	@objc private func presentFilterView() {
+		navigator?.presentFilter()
+	}
+}
+
+class InformationNavigator {
+	private let navigationController: UINavigationController
+	
+	init(navigationController: UINavigationController) {
+		self.navigationController = navigationController
+	}
+	
+	func presentFilter() {
+		let viewController = FilterViewController()
+		let filterNavigationController = UINavigationController(rootViewController: viewController)
+		navigationController.present(filterNavigationController, animated: true)
+	}
+}
+
+// todo: extract
+extension UIImage {
+	static let filterIcon = UIImage(systemName:"slider.horizontal.3")
 }
