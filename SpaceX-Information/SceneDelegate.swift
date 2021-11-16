b78e98cc68ea83c8aadfe7bcd8a067ede40b43c8
Owen Harper <owenharper98@gmail.com>
2021-11-16 21:10:04 +0000

    initial commit

---
+++ SpaceX-Information/SceneDelegate.swift
@@ -0,0 +1,57 @@
+//
+//  SceneDelegate.swift
+//  SpaceX-Information
+//
+//  Created by Owen Harper on 16/11/2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+	var window: UIWindow?
+	
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		
+		if let windowScene = scene as? UIWindowScene {
+			let window = UIWindow(windowScene: windowScene)
+			window.rootViewController = UINavigationController(rootViewController: InformationViewController()) // todo: change for splitviewcontroller or some tab bar thing
+			
+			self.window = window
+			
+			window.makeKeyAndVisible()
+		}
+	}
+	
+	// todo: review if needed
+	func sceneDidDisconnect(_ scene: UIScene) {
+		// Called as the scene is being released by the system.
+		// This occurs shortly after the scene enters the background, or when its session is discarded.
+		// Release any resources associated with this scene that can be re-created the next time the scene connects.
+		// The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+	}
+	
+	// todo: review if needed
+	func sceneDidBecomeActive(_ scene: UIScene) {
+		// Called when the scene has moved from an inactive state to an active state.
+		// Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+	}
+	
+	// todo: review if needed
+	func sceneWillResignActive(_ scene: UIScene) {
+		// Called when the scene will move from an active state to an inactive state.
+		// This may occur due to temporary interruptions (ex. an incoming phone call).
+	}
+	
+	// todo: review if needed
+	func sceneWillEnterForeground(_ scene: UIScene) {
+		// Called as the scene transitions from the background to the foreground.
+		// Use this method to undo the changes made on entering the background.
+	}
+	
+	// todo: review if needed
+	func sceneDidEnterBackground(_ scene: UIScene) {
+		// Called as the scene transitions from the foreground to the background.
+		// Use this method to save data, release shared resources, and store enough scene-specific state information
+		// to restore the scene back to its current state.
+	}
+}
---
+++ SpaceX-Information/Views/Information/InformationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InformationViewController.swift
+//  SpaceX-Information
+//
+//  Created by Owen Harper on 16/11/2021.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
