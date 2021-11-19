d1827a30bbf8701484c13355b983f8615955d3f2
Owen Harper <owenharper98@gmail.com>
2021-11-19 21:02:24 +0000

    wip: extract rocket

--- Domain/Flights/FlightRepository.swift
+++ Domain/Flights/FlightRepository.swift
@@ -4,22 +4,7 @@
 //
 //  Created by Owen Harper on 18/11/2021.
 //
+
 public protocol FlightRepository {
 	func retrieve(completion: @escaping (Result<[Flight], DomainError>) -> ())
-}
-
-// todo: extract
-
-
-// todo: extract
-public struct Rocket {
-	public let id: String
-	public let name: String
-	public let type: String
-	
-	public init(id: String, name: String, type: String) {
-		self.id = id
-		self.name = name
-		self.type = type
-	}
 }
