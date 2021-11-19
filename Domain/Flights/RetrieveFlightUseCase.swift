e3bd0cbcfbc384325475b2fa6fda51057d588bff
Owen Harper <owenharper98@gmail.com>
2021-11-19 20:36:43 +0000

    wip: add use case

---
+++ Domain/Flights/RetrieveFlightUseCase.swift
@@ -0,0 +1,22 @@
+//
+//  RetrieveFlightUseCase.swift
+//  Domain
+//
+//  Created by Owen Harper on 19/11/2021.
+//
+
+public protocol RetrieveFlightUseCase {
+	func execute(completion: @escaping (Result<[Flight], DomainError>) -> ())
+}
+
+class DefaultRetrieveFlightUseCase: RetrieveFlightUseCase {
+	private let repository: FlightRepository
+	
+	public init(repository: FlightRepository) {
+		self.repository = repository
+	}
+	
+	func execute(completion: @escaping (Result<[Flight], DomainError>) -> ()) {
+		repository.retrieve(completion: completion)
+	}
+}
--- Repository/Flights/FlightRepository.swift
+++ Repository/Flights/FlightRepository.swift
@@ -37,3 +37,4 @@ public class FlightRepository: Domain.FlightRepository {
 		}
 	}
 }
+
