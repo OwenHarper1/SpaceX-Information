843fa38c7aa92c9d5a23940d3b8253e73454a79c
Owen Harper <owenharper98@gmail.com>
2021-11-20 14:03:28 +0000

    wip: extract delegate

--- SpaceX-Information/Views/Information/InformationViewController.swift
+++ SpaceX-Information/Views/Information/InformationViewController.swift
@@ -147,21 +147,3 @@ extension InformationViewController: InformationViewModelDelegate {
 		}
 	}
 }
-
-// todo: extract
-class InformationCollectionViewCellDelegate: NSObject, UICollectionViewDelegate {
-	typealias Section = InformationViewController.Section
-	
-	private let viewModel: InformationViewModel
-	
-	init(viewModel: InformationViewModel) {
-		self.viewModel = viewModel
-	}
-	
-	func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-		guard let count = viewModel.flights?.count, count > 0,
-			  indexPath == IndexPath(item: count - 1, section: Section.flights.rawValue) else { return }
-		
-		viewModel.loadFlightInformation()
-	}
-}
